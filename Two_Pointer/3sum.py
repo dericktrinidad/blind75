1aa06d52ac49a944c1411fb59b9ef56b8d6c0f61
deric <dericktrinidad@gmail.com>
2022-06-14 18:22:40 -0400

    3sum

--- Two_Pointer/3sum.py
+++ Two_Pointer/3sum.py
@@ -1,9 +1,27 @@
 class Solution:
     def threesum(self, nums):
         sums = []
+        nums.sort()
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
+                continue
+            else:
+                l, r = i + 1, len(nums) - 1
+                while l < r:
+                    s = nums[l] + nums[r] + n
+                    if s < 0:
+                        l += 1
+                    elif s > 0:
+                        r -= 1
+                    else:
+                        sums.append([nums[l], nums[r], n])
+                        #only need to shift one pointer after solution found
+                        l += 1
+                        # if left pointer is same as prev left pointer and left less than right: keep shifting leftpointer
+                        while nums[l] == nums[l - 1] and l < r:
+                            l += 1
+        return sums
 
-        for i in nums:
-            pass
 
 if __name__ == '__main__':
     s = Solution()
