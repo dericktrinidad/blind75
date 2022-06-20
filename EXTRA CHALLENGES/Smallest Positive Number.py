916764b1700c06ed0c7e7d45ec58575e32c16cbe
dericktrinidad <51333368+dericktrinidad@users.noreply.github.com>
2022-06-20 16:24:23 -0400

    extra and 1D programming

---
+++ EXTRA CHALLENGES/Smallest Positive Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def missingPosNo(self, nums):
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if 1 <= val <= len(nums):
+                if nums[val - 1] > 0:
+                    nums[val - 1] *= -1
+                elif nums[val - 1] == 0:
+                    nums[val - 1] = -1 * (len(nums) + 1)
+
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] >= 0:
+                return i
+        return len(nums) + 1
+if __name__ == '__main__':
+    s = Solution()
+    print(s.missingPosNo([1,2,0]))
